feed03e1a5e05675f318cc6b07ac867bc90feef5
Alex <alex.alexandrov.1988@gmail.com>
2024-05-15 18:27:34 +0300

    start

--- src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
+++ src/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx
@@ -1,6 +1,6 @@
 import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
 
-import { Checkbox, CheckboxProps } from '../../'
+import { Checkbox, CheckboxProps } from '@/components/ui/checkbox'
 
 export type ControlledCheckboxProps<TFieldValues extends FieldValues> =
   UseControllerProps<TFieldValues> & Omit<CheckboxProps, 'id' | 'onChange' | 'value'>
